c62db04c789a2d1aec5781d24c0aae199ec267e8
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-07-17 17:09:45 +0200

    webpack configuration for project was added.

---
+++ app/scripts/index.js
@@ -0,0 +1,5 @@
+import 'normalize.css'
+import 'reset-css'
+import '@/stylesheets/styles.sass'
+
+console.log("in index.js !!")
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+    mode: 'development',
+    entry:{
+        main: path.resolve(__dirname, 'app/scripts/index.js')
+    },
+    output:{
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve:{
+        alias: {
+            '@': path.resolve(__dirname, 'app')
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname,'app/index.html'),
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        })
+    ],
+    module:{
+        rules: [
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    }
+}
