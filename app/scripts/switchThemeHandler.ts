789daa343a9b22548cd4d1291550ac758b965e77
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-07-22 00:51:39 +0200

    theme handler has been added.

---
+++ app/scripts/switchThemeHandler.ts
@@ -0,0 +1,23 @@
+import * as $ from 'jquery'
+
+export default class SwitchThemeHandler{
+    $checkboxSwitch: JQuery<any>
+
+    constructor(selector: String){
+        if(!selector)
+            throw new Error('No selector provided')
+        this.$checkboxSwitch = $(selector)
+        if(this.$checkboxSwitch.length === 0)
+            throw new Error('Could not find element with selector: ' + selector)
+    }
+
+    addClickHandler() : void{
+        console.log('Setting click handler for theme checkbox switch')
+        this.$checkboxSwitch.on('click', (event) => {
+            if (this.$checkboxSwitch[0].checked)
+                document.documentElement.className = 'light'
+            else
+                document.documentElement.className = 'dark'
+        })
+    }
+}
--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -2,6 +2,7 @@ import * as $ from 'jquery'
 import SearchHandler from './searchHandler.ts'
 import HeaderProfileHandler from './headerProfileHandler.ts'
 import fetchTemplates from './fetchTemplates.js'
+import SwitchThemeHandler from './switchThemeHandler.ts'
 
 const searchSelector = '[rel="js-search-container"]'
 const inputSelector = '[rel="js-search-input"]'
@@ -9,15 +10,18 @@ const buttonSelector = '[rel="js-search-button"]'
 const headerProfileSelector = '[rel="js-header-profile"]'
 const headerPopupSelector = '[rel="js-header-popup"]'
 const PopupAnyPlacesSelector = '[rel="js-popup-any-places"]'
+const switchCheckboxSelector = '[rel="js-switch-checkbox"]'
 
 fetchTemplates().then((data) => {
     console.log(data)
 
     let searchHandler = new SearchHandler(searchSelector)
     let headerProfileHandler = new HeaderProfileHandler(headerProfileSelector)
+    let switchThemeHandler = new SwitchThemeHandler(switchCheckboxSelector)
 
     searchHandler.addSubmitHandler((data) => {console.log(data)}, $(buttonSelector), $(inputSelector))
     searchHandler.addFocusHandler($(inputSelector))
     headerProfileHandler.addClickHandler($(headerPopupSelector))
     headerProfileHandler.addPopupHandler($(headerPopupSelector), $(PopupAnyPlacesSelector))
+    switchThemeHandler.addClickHandler()
 })
