407c4198c457a688ffb706c6cfc2945883525f2c
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-07-22 20:57:07 +0200

    dark theme has been added.

--- app/scripts/switchThemeHandler.ts
+++ app/scripts/switchThemeHandler.ts
@@ -9,6 +9,8 @@ export default class SwitchThemeHandler{
         this.$checkboxSwitch = $(selector)
         if(this.$checkboxSwitch.length === 0)
             throw new Error('Could not find element with selector: ' + selector)
+        //* default theme color
+        document.documentElement.className = 'light'
     }
 
     addClickHandler() : void{
