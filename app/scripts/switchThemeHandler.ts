87a24a6bf418979262aaf47822221dccc2b55604
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-07-22 23:01:36 +0200

    loacalStorage has been added.

--- app/scripts/switchThemeHandler.ts
+++ app/scripts/switchThemeHandler.ts
@@ -10,16 +10,41 @@ export default class SwitchThemeHandler{
         if(this.$checkboxSwitch.length === 0)
             throw new Error('Could not find element with selector: ' + selector)
         //* default theme color
-        document.documentElement.className = 'light'
+        if (this.#getTheme() == undefined){
+            document.documentElement.className = 'light'
+            this.$checkboxSwitch[0].checked = true
+        } else {
+            let theme = this.#getTheme()
+            document.documentElement.className = theme
+            if (theme == 'light')
+                this.$checkboxSwitch[0].checked = true
+            else
+                this.$checkboxSwitch[0].checked = false
+        }
     }
 
     addClickHandler() : void{
         console.log('Setting click handler for theme checkbox switch')
         this.$checkboxSwitch.on('click', (event) => {
-            if (this.$checkboxSwitch[0].checked)
+            if (this.$checkboxSwitch[0].checked){
+                this.#setTheme('light')
                 document.documentElement.className = 'light'
-            else
+            } else {
+                this.#setTheme('dark')
                 document.documentElement.className = 'dark'
+            }
         })
+    }
+
+    #setTheme(theme: string) : void {
+        localStorage.setItem('theme', theme)
+    }
+
+    #getTheme() : string {
+        const theme = localStorage.getItem('theme')
+        if (theme)
+            return theme
+        else
+            return undefined
     }
 }
